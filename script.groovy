80bc69ede7955f780d84abdacf1c1d6694896c33
OoOmarsala7 <omarsalah087@gmail.com>
2024-08-17 22:03:58 +0300

    script.groovy fixes

--- script.groovy
+++ script.groovy
@@ -1,12 +1,15 @@
 def test() {
-      echo "Building the application ${params.VERSION}"
-      sh 'mvn test'
+    echo "Building the application ${params.VERSION}"
+    sh 'mvn test'
 }
 
 def pack() {
-    echo "packging the application"
-    sh "mvn build-helper:parse-version versions:set \
-  -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.nextMinorVersion}.\\\${parsedVersion.incrementalVersion}\ versions:commit"
+    echo "Packaging the application"
+    sh '''
+        mvn build-helper:parse-version versions:set \
+        -DnewVersion=${parsedVersion.majorVersion}.${parsedVersion.nextMinorVersion}.${parsedVersion.incrementalVersion} \
+        versions:commit
+    '''
     def version = readFile('pom.xml') =~ '<version>(.+)</version>'
     def matcher = version[0][1]
     IMAGE_NAME = "$matcher-$BUILD_NUMBER"
@@ -14,21 +17,18 @@ def pack() {
     sh 'mvn clean package'
 }
 
-
-
 def build_con() {
     echo "Building the container"
     sh "docker build -t omarsala78/my-rep:jvm-${IMAGE_NAME} ."
 }
 
 def deploy() {
-       withCredentials([usernamePassword(credentialsId: 'docker_hub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-                        echo "Logging in to Docker Hub"
-                        sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
-                         echo "Deploying the container to Docker Hub"
-                        sh "docker build -t omarsala78/my-rep:jvm-${IMAGE_NAME} ."
-                    }
+    withCredentials([usernamePassword(credentialsId: 'docker_hub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        echo "Logging in to Docker Hub"
+        sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
+        echo "Deploying the container to Docker Hub"
+        sh "docker push omarsala78/my-rep:jvm-${IMAGE_NAME}"
+    }
 }
-
 
 return this
