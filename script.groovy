ae8c75feb68d2fd24a5a1703963d52ca43363d55
OoOmarsala7 <omarsalah087@gmail.com>
2024-08-17 23:08:12 +0300

    script.groovy fixes

--- script.groovy
+++ script.groovy
@@ -1,38 +1,45 @@
+// Declare IMAGE_NAME at a higher scope
+def IMAGE_NAME = ""
+
 def test() {
     echo "Building the application ${params.VERSION}"
     sh 'mvn test'
 }
 
 def incrementVersion() {
-     echo 'incrementing app version...'
-     sh "mvn build-helper:parse-version versions:set \
-         -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.newIncrementalVersion} versions:commit"
+    echo 'Incrementing app version...'
+    sh '''
+        mvn build-helper:parse-version versions:set \
+        -DnewVersion=${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.newIncrementalVersion} \
+        versions:commit
+    '''
     def version = readFile('pom.xml') =~ '<version>(.+)</version>'
     def matcher = version[0][1]
-    IMAGE_NAME = "$matcher-$BUILD_NUMBER"
-    
+    IMAGE_NAME = "${matcher}-${BUILD_NUMBER}"
 }
 
 def build_image() {
-    echo "building the jar file ..."
-        sh 'mvn clean package'
-
+    echo "Building the jar file ..."
+    sh 'mvn clean package'
 }
 
 def build_con() {
     echo "Building the container"
-     sh '''
+    sh '''
         #!/bin/bash
-        echo "Building Docker image with tag: omarsala78/my-rep:jvm-${IMAGE_NAME}"
-        docker build -t omarsala78/my-rep:jvm-${IMAGE_NAME} .
-        '''
+        docker build -t omarsala78/my-rep:${IMAGE_NAME} .
+    '''
+}
 
 def deploy() {
     withCredentials([usernamePassword(credentialsId: 'docker_hub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
         echo "Logging in to Docker Hub"
         sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
         echo "Deploying the container to Docker Hub"
-        sh "docker push omarsala78/my-rep:$IMAGE_NAME"
+        sh '''
+            #!/bin/bash
+            docker push omarsala78/my-rep:${IMAGE_NAME}
+        '''
     }
 }
 
