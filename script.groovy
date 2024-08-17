781ba1876bc61a744483d5e5990fa2c6368405de
OoOmarsala7 <omarsalah087@gmail.com>
2024-08-17 23:11:11 +0300

    script.groovy fixes

--- script.groovy
+++ script.groovy
@@ -1,34 +1,27 @@
-// Declare IMAGE_NAME at a higher scope
-def IMAGE_NAME = ""
-
 def test() {
     echo "Building the application ${params.VERSION}"
     sh 'mvn test'
 }
 
 def incrementVersion() {
-    echo 'Incrementing app version...'
-    sh '''
-        mvn build-helper:parse-version versions:set \
-        -DnewVersion=${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.newIncrementalVersion} \
-        versions:commit
-    '''
+     echo 'incrementing app version...'
+     sh "mvn build-helper:parse-version versions:set \
+         -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.newIncrementalVersion} versions:commit"
     def version = readFile('pom.xml') =~ '<version>(.+)</version>'
     def matcher = version[0][1]
-    IMAGE_NAME = "${matcher}-${BUILD_NUMBER}"
+    IMAGE_NAME = "$matcher-$BUILD_NUMBER"
+    
 }
 
 def build_image() {
-    echo "Building the jar file ..."
-    sh 'mvn clean package'
+    echo "building the jar file ..."
+        sh 'mvn clean package'
+
 }
 
 def build_con() {
     echo "Building the container"
-    sh '''
-        #!/bin/bash
-        docker build -t omarsala78/my-rep:${IMAGE_NAME} .
-    '''
+    sh "docker build -t omarsala78/my-rep:${IMAGE_NAME} ."
 }
 
 def deploy() {
@@ -36,10 +29,7 @@ def deploy() {
         echo "Logging in to Docker Hub"
         sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
         echo "Deploying the container to Docker Hub"
-        sh '''
-            #!/bin/bash
-            docker push omarsala78/my-rep:${IMAGE_NAME}
-        '''
+        sh "docker push omarsala78/my-rep:${IMAGE_NAME}"
     }
 }
 
